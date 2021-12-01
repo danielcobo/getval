9eb7572fa0a27b6d524b1476251aba6a9a22064f
danielcobo <danielcobo@users.noreply.github.com>
2021-12-01 19:07:28 +0100

    Consolidate into index.js

--- src/getInput.js
+++
@@ -1,8 +0,0 @@
-/**
- * Returns value of an input
- * @param {HTMLElement} $el
- * @returns {*}
- */
-module.exports = function getInput($el) {
-  return $el.value;
-};
--- src/getInput.test.js
+++
@@ -1,9 +0,0 @@
-/**
- * @jest-environment jsdom
- */
-const getInput = require('./getInput.js');
-test('Get input', function () {
-  document.body.innerHTML = '<input id="test" value="nice day">';
-  const $el = document.getElementById('test');
-  expect(getInput($el)).toStrictEqual('nice day');
-});
